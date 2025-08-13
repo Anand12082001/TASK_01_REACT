e1b95333b185d79ab772714425fdeacc0b74eb01
Anand12082001 <Anandjayakumardae@gmail.com>
2025-08-13 16:43:44 +0530

    button alter

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,10 @@ export default function ProductCard({ product, addToCart }) {
       <img src={product.image} alt={product.title} />
       <h3>{product.title}</h3>
       <p className="price">${product.price}</p>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <button className="add-to-cart-btn" onClick={() => addToCart(product)}>Add to Cart</button>
+      {/* 
+      .add-to-cart-btn {
+  margin-top: auto; */}
     </div>
   );
 }
